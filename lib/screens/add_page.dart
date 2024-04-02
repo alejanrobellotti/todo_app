1450a2694f4949e136db660bc6cf4cc9413ab551
alejanrobellotti <alejandro.bellotti@gmail.com>
2024-04-02 13:27:55 -0300

    Modificacion general

--- lib/screens/add_page.dart
+++ lib/screens/add_page.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class AddTodoPage extends StatefulWidget {
   const AddTodoPage({super.key});
@@ -8,13 +11,92 @@ class AddTodoPage extends StatefulWidget {
 }
 
 class _AddTodoPageState extends State<AddTodoPage> {
+  TextEditingController tituloControler = TextEditingController();
+  TextEditingController detalleControler = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text('Add Todo'),
+        title: Text('Add Todo'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(30),
+        children: [
+          TextField(
+            controller: tituloControler,
+            decoration: InputDecoration(hintText: 'Titulo'),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          TextField(
+            controller: detalleControler,
+            decoration: InputDecoration(hintText: 'Detalles'),
+            keyboardType: TextInputType.multiline,
+            maxLines: 6,
+            minLines: 5,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            clipBehavior: Clip.antiAlias,
+            onPressed: submintData,
+            child: Text('Submit'),
+          ),
+        ],
       ),
     );
+  }
+
+  Future<void> submintData() async {
+    // Obtener los datos del formulario
+    final texto = tituloControler.text;
+    final detalle = detalleControler.text;
+    final body = {
+      "title": texto,
+      "description": detalle,
+      "is_completed": false
+    };
+    // Enviar datos al server
+    const url = 'https://api.nstack.in/v1/todos';
+    final uri = Uri.parse(url);
+    final response = await http.post(
+      uri,
+      body: jsonEncode(body),
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    // Mostrar si fue exitosos o no y porque
+
+    if (response.statusCode == 201) {
+      tituloControler.text = '';
+      detalleControler.text = '';
+      print('Creacion Success');
+      showSuccesMessage('Creacion Success');
+    } else {
+      print('Failed Creacion Success');
+      showErrorSuccesMessage('Failed Creacion Success');
+      print(response.body);
+    }
+  }
+
+  void showSuccesMessage(String message) {
+    final snackBar = SnackBar(
+      content: Text(message),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  void showErrorSuccesMessage(String message) {
+    final snackBar = SnackBar(
+      content: Text(
+        message,
+        style: TextStyle(color: Colors.white),
+      ),
+      backgroundColor: Colors.red,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
--- lib/screens/todo_list.dart
+++ lib/screens/todo_list.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:todo_app/config/app_theme.dart';
 import 'package:todo_app/screens/add_page.dart';
+import 'package:http/http.dart' as http;
 
 class TodoListPage extends StatefulWidget {
   const TodoListPage({super.key});
@@ -10,6 +13,13 @@ class TodoListPage extends StatefulWidget {
 }
 
 class _TodoListPageState extends State<TodoListPage> {
+  List items = [];
+  @override
+  void initState() {
+    super.initState();
+    todoList();
+  }
+
   final tema = AppTheme();
   @override
   Widget build(BuildContext context) {
@@ -20,6 +30,26 @@ class _TodoListPageState extends State<TodoListPage> {
         title: const Text(
           'Todo List',
           style: TextStyle(color: Colors.white),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: todoList,
+          )
+        ],
+      ),
+      body: RefreshIndicator(
+        onRefresh: todoList,
+        child: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final item = items[index] as Map;
+            return ListTile(
+              leading: CircleAvatar(child: Text('${index + 1}')),
+              title: Text(item['title']),
+              subtitle: Text(item['description']),
+            );
+          },
         ),
       ),
       floatingActionButton: FloatingActionButton.extended(
@@ -32,5 +62,18 @@ class _TodoListPageState extends State<TodoListPage> {
   void navigateToAdPage() {
     final route = MaterialPageRoute(builder: (context) => const AddTodoPage());
     Navigator.push(context, route);
+  }
+
+  Future<void> todoList() async {
+    const url = 'https://api.nstack.in/v1/todos?page=1&limit=20';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body) as Map;
+      final resultado = json['items'] as List;
+      setState(() {
+        items = resultado;
+      });
+    } else {}
   }
 }
